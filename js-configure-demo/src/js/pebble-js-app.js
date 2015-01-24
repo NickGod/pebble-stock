feb3fd0db82a7e57e064246024a2209ff8d5e826
Quan Chau <mqchau@uci.edu>
2015-01-24 10:51:28 -0800

    finally can communicate pebble and phone

--- js-configure-demo/src/js/pebble-js-app.js
+++ js-configure-demo/src/js/pebble-js-app.js
@@ -1,14 +1,10 @@
 var initialized = false;
 var options = {};
 
-Pebble.addEventListener("ready", function() {
-  console.log("ready called!");
-  initialized = true;
-});
 
 Pebble.addEventListener("showConfiguration", function() {
   console.log("showing configuration");
-  Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html?'+encodeURIComponent(JSON.stringify(options)));
+  Pebble.openURL('http://mqchau.pythonanywhere.com/pebblestocksettings.html?'+encodeURIComponent(JSON.stringify(options)));
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
@@ -22,3 +18,94 @@ Pebble.addEventListener("webviewclosed", function(e) {
     console.log("Cancelled");
   }
 });
+
+// Set callback for the app ready event
+Pebble.addEventListener("ready",
+                        function(e) {
+                          console.log("connect! " + e.ready);
+                          console.log(e.type);
+                          // Fetch saved symbol from local storage (using
+                          // standard localStorage webAPI)
+                          symbol = localStorage.getItem("symbol");
+                          if (!symbol) {
+                            symbol = "PBL";
+                          }
+                          var isInitMsg = true;
+                          fetchStockQuote(symbol, isInitMsg);
+                        });
+
+// Set callback for appmessage events
+Pebble.addEventListener("appmessage",
+                        function(e) {
+							 var msg = {};
+							msg.init = true;
+							//msg.symbol = options.StockSymbol;
+							msg.symbol = "APPL";
+							msg.price = "12.50";
+							Pebble.sendAppMessage(msg);
+							
+                         /* console.log("message");
+                          var isInitMsg;
+                          if (e.payload.init) {
+                            isInitMsg = true;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }
+                          else if (e.payload.fetch) {
+                            isInitMsg = false;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }
+                          else if (e.payload.symbol) {
+                            symbol = e.payload.symbol;
+                            localStorage.setItem("symbol", symbol);
+                            isInitMsg = false;
+                            fetchStockQuote(symbol, isInitMsg);
+                          }*/
+                        });
+
+
+// We use the fake "PBL" symbol as default
+var defaultSymbol = "GOOG";
+var symbol = defaultSymbol;
+
+// Fetch stock data for a given stock symbol (NYSE or NASDAQ only) from markitondemand.com
+// & send the stock price back to the watch via app message
+// API documentation at http://dev.markitondemand.com/#doc
+function fetchStockQuote(symbol, isInitMsg) {
+  var response;
+  var req = new XMLHttpRequest();
+  // build the GET request
+  req.open('GET', "http://dev.markitondemand.com/Api/Quote/json?" +
+    "symbol=" + symbol, true);
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      // 200 - HTTP OK
+      if(req.status == 200) {
+        console.log(req.responseText);
+        response = JSON.parse(req.responseText);
+        var price;
+        if (response.Message) {
+          // the markitondemand API sends a response with a Message
+          // field when the symbol is not found
+          Pebble.sendAppMessage({
+            "price": "Not Found"});
+        }
+        if (response.Data) {
+          // data found, look for LastPrice
+          price = response.Data.LastPrice;
+          console.log(price);
+
+          var msg = {};
+          if (isInitMsg) {
+            msg.init = true;
+            msg.symbol = symbol;
+          }
+          msg.price = "$" + price.toString();
+          Pebble.sendAppMessage(msg);
+        }
+      } else {
+        console.log("Request returned error code " + req.status.toString());
+      }
+    }
+  };
+  req.send(null);
+}
