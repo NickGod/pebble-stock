897a5634ee2cb078abb38122af6dcea8be1dca92
Matthew Lin <darthpwner@yahoo.com>
2015-01-24 15:13:10 -0800

    Imported json and added some display features.

--- pebble-stock.py
+++ pebble-stock.py
@@ -1,7 +1,8 @@
 from flask import Flask, request    #original directory is flash
-import random     #separate directories from flask requires a new line
-#import JSON
+import random, json     #separate directories from flask requires a new line
 app = Flask(__name__)
+
+dictStock = {}
 
 @app.route('/')
 def get_stock():
@@ -17,6 +18,7 @@ def getPebbleStockSettings():
 def stockHashTable():
     getKey = request.args.get('input')
 
+    global dictStock
     dictStock = {'MMM': '3M Co', 'AXP': 'American Express Co', 'T': 'AT&T Inc', 'BA': 'Boeing Co', 'CAT': 'Caterpillar Inc', 'CVX': 'Chevron Corp',
     'CSCO': 'Cisco Systems Inc', 'KO': 'The Coca-Cola Co', 'DIS': 'Walt Disney Co', 'DD': 'E I du Pont de Nemours and Co', 'XON': 'Exxon Mobil Corp',
     'GE': 'General Electric Co', 'GS': 'Goldman Sachs Group Inc', 'HD': 'Home Depot Inc', 'IBM': 'International Business Machines Corp', 'INTC': 'Intel Corp',
@@ -33,14 +35,20 @@ def stockHashTable():
     else:   #Give an error message if there is no input or if input is incorrect
         return "Please input a Symbol"
 
+
 @app.route('/random')
 def generateRand():
     rand = (random.uniform(10.0, 100.0))    #Generate a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
-    #Return the rounded value of the 'rand' variable as a string
     return str(format(rand, '.2f'))
 
+@app.route('/json')
+def display():
+    #symbol = request.args.get('symbol')
+    struct = {"Data":{"Status":"SUCCESS","Name":"Google Inc","Symbol":"GOOGL","LastPrice":541.98,"Change":4.68000000000006,"ChangePercent":0.871021775544401,"Timestamp":"Fri Jan 23 15:59:00 UTC-05:00 2015","MarketCap":183745311480,"Volume":215950,"ChangeYTD":530.66,"ChangePercentYTD":2.1331926280481,"High":545.2,"Low":536.82,"Open":538.6}}
+    return json.dumps(struct)
+
 def main():
-    stockHashTable()
+    display()
 
 if __name__ == "__main__":
     main()
