e3cd2c56b328cd4d3f023e271a2670cc5e9f9468
Matthew Lin <darthpwner@yahoo.com>
2015-01-24 18:09:01 -0800

    Made some more changes to the display
    Fixed the display function and completely randomized the change in the percentage.

--- pebble-stock.py
+++ pebble-stock.py
@@ -1,8 +1,13 @@
 from flask import Flask, request    #original directory is flash
-import random, json     #separate directories from flask requires a new line
+import random, json, time     #separate directories from flask requires a new line
 app = Flask(__name__)
 
-dictStock = {}
+dictStock = {'MMM': '3M Co', 'AXP': 'American Express Co', 'T': 'AT&T Inc', 'BA': 'Boeing Co', 'CAT': 'Caterpillar Inc', 'CVX': 'Chevron Corp',
+    'CSCO': 'Cisco Systems Inc', 'KO': 'The Coca-Cola Co', 'DIS': 'Walt Disney Co', 'DD': 'E I du Pont de Nemours and Co', 'XON': 'Exxon Mobil Corp',
+    'GE': 'General Electric Co', 'GS': 'Goldman Sachs Group Inc', 'HD': 'Home Depot Inc', 'IBM': 'International Business Machines Corp', 'INTC': 'Intel Corp',
+    'JNJ': 'Johnson & Johnson', 'JPM': 'JPMorgan Chase and Co', 'MCD': "McDonald's Corp", 'MRK': 'Merck & Co Inc', 'MSFT': 'Microsoft Corp', 'NKE': 'Nike Inc',
+    'PFE': 'Pfizer Inc', 'PG': 'Procter & Gamble Co', 'TRV': 'Travelers Companies Inc', 'UTX': 'United Technologies Corp', 'UNH': 'UnitedHealth Group Inc',
+    'VZ': 'Verizon Communications Inc', 'V': 'Visa Inc', 'WMT': 'Wal-Mart Stores Inc', 'GOOG': 'Google Inc', 'AAPL': 'Apple Inc'}
 
 @app.route('/')
 def get_stock():
@@ -18,14 +23,6 @@ def getPebbleStockSettings():
 def stockHashTable():
     getKey = request.args.get('input')
 
-    global dictStock
-    dictStock = {'MMM': '3M Co', 'AXP': 'American Express Co', 'T': 'AT&T Inc', 'BA': 'Boeing Co', 'CAT': 'Caterpillar Inc', 'CVX': 'Chevron Corp',
-    'CSCO': 'Cisco Systems Inc', 'KO': 'The Coca-Cola Co', 'DIS': 'Walt Disney Co', 'DD': 'E I du Pont de Nemours and Co', 'XON': 'Exxon Mobil Corp',
-    'GE': 'General Electric Co', 'GS': 'Goldman Sachs Group Inc', 'HD': 'Home Depot Inc', 'IBM': 'International Business Machines Corp', 'INTC': 'Intel Corp',
-    'JNJ': 'Johnson & Johnson', 'JPM': 'JPMorgan Chase and Co', 'MCD': "McDonald's Corp", 'MRK': 'Merck & Co Inc', 'MSFT': 'Microsoft Corp', 'NKE': 'Nike Inc',
-    'PFE': 'Pfizer Inc', 'PG': 'Procter & Gamble Co', 'TRV': 'Travelers Companies Inc', 'UTX': 'United Technologies Corp', 'UNH': 'UnitedHealth Group Inc',
-    'VZ': 'Verizon Communications Inc', 'V': 'Visa Inc', 'WMT': 'Wal-Mart Stores Inc', 'GOOG': 'Google Inc', 'AAPL': 'Apple Inc'}
-
     #Give an error message if symbol is not in dictionary
     if getKey is not None:   #Check if the input is valid
         if dictStock.has_key(getKey):
@@ -36,15 +33,54 @@ def stockHashTable():
         return "Please input a Symbol"
 
 
-@app.route('/random')
+@app.route('/randomPrice')
 def generateRand():
-    rand = (random.uniform(10.0, 100.0))    #Generate a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
-    return str(format(rand, '.2f'))
+    rand = (random.uniform(10.00, 100.00))    #Generate a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
+    return round(rand, 2)
+
+@app.route('/change')
+def change():
+    r_change = (random.uniform(-100.00, 100.00))
+    return round(r_change, 2)
+
+def changePercent():
+    r_changePercent = (random.uniform(-7.00, 7.00))
+    return round(r_changePercent, 2)
+
+def marketCap():
+    r_marketCap = (random.uniform(100000000000, 200000000000))
+    return round(r_marketCap, 2)
+
+def additionalChanges():
+    r_additional_changes = random.uniform(500, 550)
+    return round(r_additional_changes, 2)
+
+def low():
+    r_low = random.uniform(500, 525)
+    return round(r_low, 2)
+
+def high():
+    r_high = random.uniform(525, 550)
+    return round(r_high, 2)
+
+def changePercentYTD():
+    r_change_percent_YTD = random.uniform(-5, 5)
+    return round(r_change_percent_YTD, 2)
+
+def volume():
+    r_volume = (random.uniform(200000, 250000))
+    return round(r_volume, 2)
+
+def timestamp():
+    now = time.strftime("%c")
+    return ("%s"  % now )
 
 @app.route('/json')
 def display():
     symbol = request.args.get('symbol')
-    struct = {"Data":{"Status":"SUCCESS","Name":"[dictStock[:symbol]]","Symbol":"[symbol]","LastPrice":541.98,"Change":4.68000000000006,"ChangePercent":0.871021775544401,"Timestamp":"Fri Jan 23 15:59:00 UTC-05:00 2015","MarketCap":183745311480,"Volume":215950,"ChangeYTD":530.66,"ChangePercentYTD":2.1331926280481,"High":545.2,"Low":536.82,"Open":538.6}}
+    struct = {"Data":{"Status":"SUCCESS","Name": dictStock[symbol],"Symbol": symbol,"LastPrice": generateRand(),"Change": change(),
+        "ChangePercent": changePercent(),"Timestamp": timestamp(),"MarketCap": marketCap(),"Volume": volume(),"ChangeYTD": additionalChanges(),
+        "ChangePercentYTD": changePercentYTD(),"High": high(),"Low": low(),"Open":additionalChanges()}}
     return json.dumps(struct)
 
 def main():
